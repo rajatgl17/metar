e8d6f7122d52029073a5907b2519c70ed62a5c1a
Rajat Goel <rajatg@azuga.com>
2019-03-12 17:52:05 +0530

    spring project initialized

---
+++ src/main/java/com/anaek/metar/MetarApplication.java
@@ -0,0 +1,13 @@
+package com.anaek.metar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetarApplication.class, args);
+	}
+
+}
